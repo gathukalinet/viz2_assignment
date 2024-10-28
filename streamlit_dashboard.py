b48bafccf1a97f0650f96568d2bdd1f1a23bda78
gathukalinet <lynettg05@gmail.com>
2024-10-28 15:14:55 +0300

    Adding the plots

--- streamlit_dashboard.py
+++ streamlit_dashboard.py
@@ -11,11 +11,52 @@ def load_data():
 emp_df = load_data()
 
 # Simple Streamlit app using Plotly
-st.title("Streamlit Dashboard with Plotly")
-st.write("This is a simple example dashboard with Plotly visualizations.")
+st.title("Employment Trends Analysis in Kenya ")
+st.write("Total Inactive Population by Year and Sex.")
 
 grouped_inactive = emp_df.groupby(['year', 'sex'])['total_inactive_population'].sum().reset_index()
 grouped_inactive['total_inactive_population'] = grouped_inactive['total_inactive_population'] / 1000000
 
 fig = px.line(grouped_inactive, x='year', y='total_inactive_population', color='sex')
 st.plotly_chart(fig)
+
+grouped_inactiveTotal = emp_df.groupby(['sex'])['total_inactive_population'].sum().reset_index()
+grouped_inactiveTotal['total_inactive_population'] = grouped_inactiveTotal['total_inactive_population'] / 1000000
+
+st.write("Total Inactive Population by Sex.")
+fig1 =px.pie(grouped_inactive, values='total_inactive_population', names='sex', color='sex', 
+       labels={'sex': 'sex', 'total_inactive_population': 'total_inactive_population'})
+st.plotly_chart(fig1)
+
+
+grouped_unemployed=emp_df.groupby(['year', 'sex'])['total_unemployed_population'].sum().reset_index()
+grouped_unemployed['total_unemployed_population'] = grouped_unemployed['total_unemployed_population'] / 1000000
+
+st.write("Total Unemployed Population by Year and Sex.")
+fig2 = px.line(grouped_unemployed, x='year', y='total_unemployed_population', color='sex')
+st.plotly_chart(fig2)
+
+grouped_unemployedTotal = emp_df.groupby(['sex'])['total_unemployed_population'].sum().reset_index()
+grouped_unemployedTotal['total_unemployed_population'] = grouped_unemployedTotal['total_unemployed_population'] / 1000000
+
+st.write("Total Unemployed Population by Sex.")
+fig3 = px.pie(grouped_unemployed, values='total_unemployed_population', names='sex', color='sex', 
+       labels={'sex': 'sex', 'total_unemployed_population': 'total_unemployed_population'})
+st.plotly_chart(fig3)
+
+grouped_employed=emp_df.groupby(['year', 'sex'])['total_employed_population'].sum().reset_index()
+grouped_employed['total_employed_population'] = grouped_employed['total_employed_population'] / 1000000
+
+st.write("Total Employed Population by Year and Sex.")
+fig4 = px.line(grouped_employed, x='year', y='total_employed_population', color='sex')
+st.plotly_chart(fig4)
+
+grouped_employedTotal=emp_df.groupby(['sex'])['total_employed_population'].sum().reset_index()
+grouped_employedTotal['total_employed_population'] = grouped_employedTotal['total_employed_population'] / 1000000
+
+st.write("Total Employed Population by Sex.")
+fig5 = px.pie(grouped_employed, values='total_employed_population', names='sex', color='sex', 
+       labels={'sex': 'sex', 'total_employed_population': 'total_employed_population'})
+st.plotly_chart(fig5)   
+
+
