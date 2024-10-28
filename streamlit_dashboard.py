81a7c6bb7ef98ec2d9a5f04da6a1b96155e1fb64
gathukalinet <lynettg05@gmail.com>
2024-10-28 14:07:59 +0300

    updating the versions

--- streamlit_dashboard.py
+++ streamlit_dashboard.py
@@ -6,13 +6,16 @@ import plotly.express as px
 # Load the dataset
 @st.cache_data
 def load_data():
-    return pd.read_csv(r'C:\Users\User\Desktop\Streamlit and Dash Projects\Visualizations2 Assignment\dataset.csv')
+    return pd.read_csv('C:\Users\User\Desktop\Streamlit and Dash Projects\Visualizations2 Assignment\dataset.csv')
 
-data = load_data()
+emp_df = load_data()
 
 # Simple Streamlit app using Plotly
 st.title("Streamlit Dashboard with Plotly")
 st.write("This is a simple example dashboard with Plotly visualizations.")
-   
-fig = px.line(data, x="year", y="population", color="sex", title="Population Over Time")
+
+grouped_inactive = emp_df.groupby(['year', 'sex'])['total_inactive_population'].sum().reset_index()
+grouped_inactive['total_inactive_population'] = grouped_inactive['total_inactive_population'] / 1000000
+
+fig = px.line(grouped_inactive, x='year', y='total_inactive_population', color='sex')
 st.plotly_chart(fig)
